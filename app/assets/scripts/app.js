35a7cc61a8a563fcc7c3392d45d9afc6c5c9b394
Daniel Beff <daniel.beff@gmail.com>
2016-07-01 15:06:29 -0300

    include json viewer example

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -4,11 +4,11 @@
 
 const Axios = require('axios');
 const $ = require("jquery");
-const {
-    ipcMain
-} = require('electron');
-
-console.log(ipcMain);
+jQuery = $;
+require("jquery.json-viewer/json-viewer/jquery.json-viewer");
+// const {ipcMain} = require('electron');
+//
+// console.log(ipcMain);
 
 
 $(".bar .input-button").click(function() {
@@ -16,16 +16,17 @@ $(".bar .input-button").click(function() {
     const action = $(".bar .input-action").val();
     const url = $(".bar .input-url").val();
 
-    $(".result").html("Carregando....");
-    console.log("Carregando...");
+    $('#json-renderer').html("Carregando....");
 
     Axios({
         method: action,
         url: url
     }).then(function(response) {
-        $(".result").html(JSON.stringify(response));
+        // $(".result").html(JSON.stringify(response));
+        $('#json-renderer').jsonViewer(response);
     })
     .catch(function(error) {
-        $(".result").html(JSON.stringify(error));
+        // $(".result").html(JSON.stringify(error));
+        $('#json-renderer').jsonViewer(error);
     });
 })
