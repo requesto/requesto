c0b6a67feb5911d03dc538f915d4c970fed2669f
Daniel Beff <daniel.beff@gmail.com>
2016-07-01 00:46:33 -0300

    include test request with axios

--- renderer.js
+++ renderer.js
@@ -1,3 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+let Axios = require('axios');
+
+console.log(Axios);
+
+
+Axios.get('http://beta.json-generator.com/api/json/get/4JVFHRAHZ')
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
