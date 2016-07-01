99bc9018a423a56e2eea059761ea5528bc585fab
Daniel Beff <daniel.beff@gmail.com>
2016-07-01 01:49:30 -0300

    Add icon and test some electron features

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,10 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-let Axios = require('axios');
-
-console.log(Axios);
-
+const Axios = require('axios');
 
 Axios.get('http://beta.json-generator.com/api/json/get/4JVFHRAHZ')
     .then(function(response) {
