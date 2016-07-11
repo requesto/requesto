7952207d71eff7c42422f424776ef31c30d33ae4
Daniel Beff <daniel.beff@gmail.com>
2016-07-11 02:36:33 -0300

    create a footer component

---
+++ app/assets/scripts/components/footer.js
@@ -0,0 +1,38 @@
+import React,{Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+class Footer extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="component-footer">
+                We are using node 2
+                <script>
+                    document.write(process.versions.node)
+                </script>, Chromium
+                <script>
+                    document.write(process.versions.chrome)
+                </script>, and Electron
+                <script>
+                    document.write(process.versions.electron)
+                </script>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps({}){
+    return {};
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({},dispatch);
+}
+
+export default connect (mapStateToProps,mapDispatchToProps)(Footer);
