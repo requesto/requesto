eff92727211e4fe777c44244c3058367b233418f
Daniel Beff <daniel.beff@gmail.com>
2016-07-13 03:17:12 -0300

    improve workspace tab position and setup json viewer

--- app/assets/scripts/components/workspace.js
+++ app/assets/scripts/components/workspace.js
@@ -3,9 +3,10 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
 import Axios from 'axios';
-import $ from 'jquery';
-//import "jquery.json-viewer/json-viewer/jquery.json-viewer";
-
+import $ from "jquery";
+import jQuery from "jquery";
+window.$ = $;
+window.jQuery = jQuery;
 
 class Workspace extends Component {
 
@@ -27,34 +28,35 @@ class Workspace extends Component {
             method: action,
             url: url
         }).then(function(response) {
-            $(".result").html(JSON.stringify(response));
-            //$('#json-renderer').jsonViewer(response);
+            $('#json-renderer').jsonViewer(response);
         })
         .catch(function(error) {
-            $(".result").html(JSON.stringify(error));
-            //$('#json-renderer').jsonViewer(error);
+            $('#json-renderer').jsonViewer(error);
         });
     }
 
     render() {
         return (
             <div className="component-workspace">
-                <div className="component-tabs">
-                    <ul className="tabs">
-                        <li className="tab active">user/token</li>
-                        <li className="tab">Aba 4</li>
-                        <li className="tab">Aba 3</li>
-                    </ul>
-                </div>
-                <div className="bar">
-                    <div className="columm action">
-                        <input className="input-action" type="text" name="action" placeholder="GET" defaultValue="get" />
+                <div className="component-workspace-top">
+                    <div className="component-tabs">
+                        <ul className="tabs">
+                            <li className="tab active">user/token</li>
+                            <li className="tab">Aba 4</li>
+                            <li className="tab">Aba 3</li>
+                            <li className="tab"> + </li>
+                        </ul>
                     </div>
-                    <div className="columm url">
-                        <input className="input-url" name="url" placeholder="http://beta.json-generator.com/api/json/get/4JVFHRAHZ" defaultValue="http://beta.json-generator.com/api/json/get/4JVFHRAHZ" />
-                    </div>
-                    <div className="columm button">
-                        <input className="input-button" onClick={this.onClickSend} type="button" name="send" defaultValue="Send" />
+                    <div className="bar">
+                        <div className="columm action">
+                            <input className="input-action" type="text" name="action" placeholder="GET" defaultValue="get" />
+                        </div>
+                        <div className="columm url">
+                            <input className="input-url" name="url" placeholder="http://beta.json-generator.com/api/json/get/4JVFHRAHZ" defaultValue="http://beta.json-generator.com/api/json/get/4JVFHRAHZ" />
+                        </div>
+                        <div className="columm button">
+                            <input className="input-button" onClick={this.onClickSend} type="button" name="send" defaultValue="Send" />
+                        </div>
                     </div>
                 </div>
                 <div className="result">
