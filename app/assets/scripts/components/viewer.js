4a168a2197a8e85fb18c2fdc63a49ddad27a30a9
Daniel Beff <daniel.beff@gmail.com>
2016-07-19 19:38:01 -0300

    change component behavior

--- app/assets/scripts/components/viewer.js
+++ app/assets/scripts/components/viewer.js
@@ -7,24 +7,14 @@ class Viewer extends Component {
     constructor(props){
         super(props);
         this.state = {};
-        this.renderViewers = this.renderViewers.bind(this);
-    }
-
-    renderViewers(data,index){
-        return (
-            <li className="item" key={"viewer-" + data.name}>
-                <h1>{data.name}</h1>
-                <pre id="json-renderer"></pre>
-            </li>
-        )
     }
 
     render() {
         return (
             <div className="component-viewer">
-                <ul clssName="list">
-                    {this.props.tabs.map(this.renderViewers)}
-                </ul>
+                <pre id="json-renderer">
+                    json!
+                </pre>
             </div>
         );
     }
