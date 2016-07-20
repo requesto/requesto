1f5872b21acf4a6d717367fb7372796ea321da45
Daniel Beff <daniel.beff@gmail.com>
2016-07-20 15:16:08 -0300

    create index component

---
+++ app/assets/scripts/components/index.js
@@ -0,0 +1,37 @@
+import React,{Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import Sidebar from "./sidebar";
+import Workspace from "./workspace";
+import Footer from "./footer";
+
+class Index extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="component-index">
+                <div className="main">
+                    <Sidebar />
+                    <Workspace />
+                </div>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+function mapStateToProps({}){
+    return {};
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({},dispatch);
+}
+
+export default connect (mapStateToProps,mapDispatchToProps)(Index);
