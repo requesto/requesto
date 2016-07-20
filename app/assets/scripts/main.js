ec9f3471df59ca9198a40b9947521d24e55b595b
Daniel Beff <daniel.beff@gmail.com>
2016-07-20 15:15:50 -0300

    include index component route

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -9,6 +9,7 @@ import Reducers from './reducers';
 
 // Components
 import App from './components/app';
+import Index from './components/index';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 const routerHistory = useRouterHistory(createHashHistory)({queryKey:false})
@@ -17,7 +18,9 @@ const devTools = window.devToolsExtension ? window.devToolsExtension() : f => f;
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(Reducers,devTools)}>
         <Router history={routerHistory} onUpdate={() => window.scrollTo(0, 0)}>
-            <Route path="/" component={App} />
+            <Route path="/" component={App}>
+                <IndexRoute component={Index}/>
+            </Route>
         </Router>
     </Provider>
 ,document.querySelector('.component-root'));
