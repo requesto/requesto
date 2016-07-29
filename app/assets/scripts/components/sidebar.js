f668f1636ea527801dce6b3e08df8a4aede64963
Daniel Beff <daniel.beff@gmail.com>
2016-07-29 00:42:40 -0300

    create folder itens

--- app/assets/scripts/components/sidebar.js
+++ app/assets/scripts/components/sidebar.js
@@ -8,27 +8,93 @@ class Sidebar extends Component {
     constructor(props){
         super(props);
         this.state = {};
+        this.folderClickHandler = this.folderClickHandler.bind(this);
+    }
+
+    folderClickHandler(e){
+        const folder = $(e.currentTarget);
+        const folderList = folder.next(".folder-itens");
+        folder.toggleClass("-active");
+        if (folderList.is("ul")){
+            folderList.toggleClass("-open");
+        }
     }
 
     render() {
         return (
             <div className="component-sidebar">
+                <div className="brand">
+                    Requesto
+                    <div className="name"></div>
+                    <div className="icon"></div>
+                </div>
                 <ul className="folder-list">
-                    <li className="item">
+                    <li className="folder" onClick={this.folderClickHandler}>
                         <div className="icon type"></div>
-                        <div className="name">Test 1</div>
+                        <div className="name">Gapp</div>
                         <div className="description">2 endpoints</div>
                     </li>
-                    <li className="item active">
+                    <ul className="folder-itens">
+                        <li className="item">
+                            <div className="icon type"></div>
+                            <div className="name">User/token</div>
+                            <div className="description">2 endpoints</div>
+                        </li>
+                        <li className="item">
+                            <div className="icon type"></div>
+                            <div className="name">Test 1</div>
+                            <div className="description">2 endpoints</div>
+                        </li>
+                        <li className="item">
+                            <div className="icon type"></div>
+                            <div className="name">Test 1</div>
+                            <div className="description">2 endpoints</div>
+                        </li>
+                    </ul>
+                    <li className="folder" onClick={this.folderClickHandler}>
                         <div className="icon type"></div>
                         <div className="name">Test</div>
                         <div className="description">2 endpoints</div>
                     </li>
-                    <li className="item">
+                    <ul className="folder-itens">
+                        <li className="item">
+                            <div className="icon type"></div>
+                            <div className="name">User/token</div>
+                            <div className="description">2 endpoints</div>
+                        </li>
+                        <li className="item">
+                            <div className="icon type"></div>
+                            <div className="name">Test 1</div>
+                            <div className="description">2 endpoints</div>
+                        </li>
+                        <li className="item">
+                            <div className="icon type"></div>
+                            <div className="name">Test 1</div>
+                            <div className="description">2 endpoints</div>
+                        </li>
+                    </ul>
+                    <li className="folder" onClick={this.folderClickHandler}>
                         <div className="icon type"></div>
                         <div className="name">Test</div>
                         <div className="description">2 endpoints</div>
                     </li>
+                    <ul className="folder-itens">
+                        <li className="item">
+                            <div className="icon type"></div>
+                            <div className="name">User/token</div>
+                            <div className="description">2 endpoints</div>
+                        </li>
+                        <li className="item">
+                            <div className="icon type"></div>
+                            <div className="name">Test 1</div>
+                            <div className="description">2 endpoints</div>
+                        </li>
+                        <li className="item">
+                            <div className="icon type"></div>
+                            <div className="name">Test 1</div>
+                            <div className="description">2 endpoints</div>
+                        </li>
+                    </ul>
                 </ul>
             </div>
         );
