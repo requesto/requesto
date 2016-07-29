3f124ca47c63bbf86cdb4fe16308ac521ecc4b07
Daniel Beff <daniel.beff@gmail.com>
2016-07-29 01:30:34 -0300

    resolve metadata

--- app/assets/scripts/components/editors.js
+++ app/assets/scripts/components/editors.js
@@ -21,23 +21,31 @@ class Editors extends Component {
         const action =  editor.find(".bar .select-action").val();
         const url =  editor.find(".bar .input-url").val();
         const jsonViewer = editor.find(".json-renderer");
-
+        const statusLabel = editor.find(".field.status .value");
+        const timeLabel = editor.find(".field.time .value");
+        const startTime = new Date().getTime();
         jsonViewer.html("Carregando....");
 
         Axios({
             method: action,
             url: url
         }).then(function(response) {
+            const duration = new Date().getTime() - startTime;
             jsonViewer.jsonViewer(response);
+            statusLabel.html(response.status).removeClass("-error");
+            timeLabel.html(duration + "ms");
         })
         .catch(function(error) {
+            const duration = new Date().getTime() - startTime;
             jsonViewer.jsonViewer(error);
+            statusLabel.html(error.status).addClass("-error");
+            timeLabel.html(duration + "ms");
         });
     }
 
     renderEditors(data,index){
         return (
-            <li className="editor" key={"editor-" + data.name}>
+            <li className="editor" key={"editor-" + data.name + "-" + index}>
                 <div className="top">
                     <div className="bar">
                         <div className="columm action">
@@ -56,6 +64,16 @@ class Editors extends Component {
                         </div>
                         <div className="columm button">
                             <input className="input-button" onClick={this.onClickSend} type="button" name="send" defaultValue="Send" />
+                        </div>
+                    </div>
+                    <div className="metadata">
+                        <div className="field status">
+                            <span className="label">status:</span>
+                            <span className="value">-</span>
+                        </div>
+                        <div className="field time">
+                            <span className="label">status:</span>
+                            <span className="value">-</span>
                         </div>
                     </div>
                 </div>
