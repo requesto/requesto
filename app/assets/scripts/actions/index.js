4224c62cb3df14e310062aa9e7c8c1403ce8cfc8
Daniel Beff <daniel.beff@gmail.com>
2016-07-29 01:30:51 -0300

    remove random number from tabs

--- app/assets/scripts/actions/index.js
+++ app/assets/scripts/actions/index.js
@@ -1,10 +1,12 @@
 export const TAB_ADD = 'TAB_ADD';
 
+//name: "untitled "+ Math.floor((Math.random() * 100) + 1),
+
 export function tabAdd() {
     return {
         type: TAB_ADD,
         payload: {
-            name: "untitled "+ Math.floor((Math.random() * 100) + 1),
+            name: "untitled",
             url: ""
         }
     }
