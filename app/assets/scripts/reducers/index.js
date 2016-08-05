02041075d6d14408006e1ec638cb418e4519e12c
Daniel Beff <daniel.beff@gmail.com>
2016-08-05 14:58:13 -0300

    add modal reducer

--- app/assets/scripts/reducers/index.js
+++ app/assets/scripts/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import Tabs from './tabs';
 import Folders from './folders';
+import Modal from './modal';
 
 const Reducers = combineReducers({
     tabs: Tabs,
     folders: Folders,
+    modal: Modal,
 });
 
 export default Reducers;
