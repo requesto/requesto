e29f37adc14076131e5a596b7221869759b5b883
Daniel Beff <daniel.beff@gmail.com>
2016-08-09 23:19:25 -0300

    update modal action

--- app/assets/scripts/reducers/modal.js
+++ app/assets/scripts/reducers/modal.js
@@ -1,9 +1,9 @@
-import {MODAL_OPEN} from '../actions/index';
+import {MODAL} from '../actions/index';
 
 export default function(state = [],action) {
     switch (action.type){
-        case MODAL_OPEN:
-            console.log("MODAL_OPEN reducer",action.payload);
+        case MODAL:
+            console.log("MODAL reducer",action.payload);
             return action.payload;
     }
     return state;
