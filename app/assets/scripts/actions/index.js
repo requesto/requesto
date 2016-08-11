27293d049533b134cc10c5c623debea51c232556
Daniel Beff <daniel.beff@gmail.com>
2016-08-11 00:17:08 -0300

    create folderAddEndpoint action

--- app/assets/scripts/actions/index.js
+++ app/assets/scripts/actions/index.js
@@ -1,5 +1,6 @@
 export const TAB_ADD = 'TAB_ADD';
 export const FOLDER_ADD = 'FOLDER_ADD';
+export const FOLDER_ADD_ENDPOINT = 'FOLDER_ADD_ENDPOINT';
 export const MODAL = 'MODAL';
 
 export function tabAdd(name = "untitled") {
@@ -16,11 +17,24 @@ export function folderAdd(name) {
         type: FOLDER_ADD,
         payload: {
             name: name,
+            id: Math.random().toString(36).substr(2, 9),
             items: [{
                 name: "url",
                 description: "endpoint description",
                 type:"GET"
             }]
+        }
+    }
+}
+
+export function folderAddEndpoint(folder,name){
+    console.log(folder,name);
+    return{
+        type: FOLDER_ADD_ENDPOINT,
+        payload: {
+            name: name,
+            folderId: folder
+
         }
     }
 }
