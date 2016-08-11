2251db8aa1e3ca320af3238e9bef2697be1ddfc8
Daniel Beff <daniel.beff@gmail.com>
2016-08-11 00:18:37 -0300

    remove console log

--- app/assets/scripts/reducers/modal.js
+++ app/assets/scripts/reducers/modal.js
@@ -3,7 +3,6 @@ import {MODAL} from '../actions/index';
 export default function(state = [],action) {
     switch (action.type){
         case MODAL:
-            console.log("MODAL reducer",action.payload);
             return action.payload;
     }
     return state;
