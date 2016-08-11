8dd16d26e15ab6e4be75b7cdffa7030af9dbdc4a
Daniel Beff <daniel.beff@gmail.com>
2016-08-11 00:19:11 -0300

    remove console log

--- app/assets/scripts/reducers/tabs.js
+++ app/assets/scripts/reducers/tabs.js
@@ -3,7 +3,6 @@ import {TAB_ADD} from '../actions/index';
 export default function(state = [],action) {
     switch (action.type){
         case TAB_ADD:
-            console.log("TAB_ADD reducer",action.payload);
             return [...state,action.payload];
     }
     return state;
