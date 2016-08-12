576116475f51be09d4a34d64ca343fd79f63601c
Daniel Beff <daniel.beff@gmail.com>
2016-08-12 04:09:02 -0300

    create initial setup

---
+++ app/assets/scripts/libs/setup.js
@@ -0,0 +1,14 @@
+export const initialSetup = {
+    user: "",
+    folders: [{
+        name:"Folder",
+        id: 1,
+        items:[
+        {
+            name:"/user",
+            description:"Create new user",
+            type:"POST",
+            url:"http://beta.json-generator.com/api/json/get/4JVFHRAHZ"
+        }]
+    }]
+}
