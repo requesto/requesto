f816e4ad7ea6f4342e8402bbba43937bb649ed7a
Daniel Beff <daniel.beff@gmail.com>
2016-08-16 17:23:15 -0300

    call folderItemDelete in delete button

--- app/assets/scripts/components/sidebar.js
+++ app/assets/scripts/components/sidebar.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {folderAdd,tabAdd,modal} from "../actions/index";
+import {folderAdd,folderItemDelete,tabAdd,modal} from "../actions/index";
 
 class Sidebar extends Component {
 
@@ -13,6 +13,7 @@ class Sidebar extends Component {
         this.renderFolders = this.renderFolders.bind(this);
         this.renderItems = this.renderItems.bind(this);
         this.itemClickHandler = this.itemClickHandler.bind(this);
+        this.itemClickDeleteHandler = this.itemClickDeleteHandler.bind(this);
     }
 
     folderAddClickHandler(e){
@@ -35,7 +36,12 @@ class Sidebar extends Component {
     }
 
     itemClickDeleteHandler(e){
-        alert("Item will be deleted");
+        // alert("Item will be deleted");
+        const item = $(e.currentTarget).closest(".item");
+        const itemIndex = item.attr("data-id");
+        const folder = item.closest(".folder");
+        const folderIndex = folder.attr("data-id");
+        this.props.folderItemDelete(folderIndex,itemIndex);
         e.stopPropagation();
         e.preventDefault();
     }
@@ -48,12 +54,12 @@ class Sidebar extends Component {
 
     renderItems(data,index){
         return(
-            <li className="item" key={"item-" + data.name + "-" + index } onClick={this.itemClickHandler}>
+            <li className="item" data-id={index} key={"item-" + data.name + "-" + index } onClick={this.itemClickHandler}>
                 <div className="name">{data.name}</div>
                 <div className="description">{data.description}</div>
                 <div className={"type " + " -" + data.type.toLowerCase() }>{data.type}</div>
-                <div className="actions" onClick={this.itemClickEditHandler}>
-                    <div className="edit">
+                <div className="actions">
+                    <div className="edit" onClick={this.itemClickEditHandler}>
                         <svg x="0px" y="0px" viewBox="0 0 469.331 469.331" width="100%" height="100%">
                             <path d="M438.931,30.403c-40.4-40.5-106.1-40.5-146.5,0l-268.6,268.5c-2.1,2.1-3.4,4.8-3.8,7.7l-19.9,147.4   c-0.6,4.2,0.9,8.4,3.8,11.3c2.5,2.5,6,4,9.5,4c0.6,0,1.2,0,1.8-0.1l88.8-12c7.4-1,12.6-7.8,11.6-15.2c-1-7.4-7.8-12.6-15.2-11.6   l-71.2,9.6l13.9-102.8l108.2,108.2c2.5,2.5,6,4,9.5,4s7-1.4,9.5-4l268.6-268.5c19.6-19.6,30.4-45.6,30.4-73.3   S458.531,49.903,438.931,30.403z M297.631,63.403l45.1,45.1l-245.1,245.1l-45.1-45.1L297.631,63.403z M160.931,416.803l-44.1-44.1   l245.1-245.1l44.1,44.1L160.931,416.803z M424.831,152.403l-107.9-107.9c13.7-11.3,30.8-17.5,48.8-17.5c20.5,0,39.7,8,54.2,22.4   s22.4,33.7,22.4,54.2C442.331,121.703,436.131,138.703,424.831,152.403z"/>
                         </svg>
@@ -78,7 +84,7 @@ class Sidebar extends Component {
         var items = ('items' in data)? data.items : [];
 
         return(
-            <li className="folder" key={"folder-" + data.name + "-" + index }>
+            <li className="folder" data-id={index} key={"folder-" + data.name + "-" + index }>
                 <div className="cover" onClick={this.folderClickHandler}>
                     <div className="icon type"></div>
                     <div className="name">{data.name}</div>
@@ -123,7 +129,7 @@ function mapStateToProps({folders}){
 }
 
 function mapDispatchToProps(dispatch){
-    return bindActionCreators({folderAdd,tabAdd,modal},dispatch);
+    return bindActionCreators({folderAdd,folderItemDelete,tabAdd,modal},dispatch);
 }
 
 export default connect (mapStateToProps,mapDispatchToProps)(Sidebar);
