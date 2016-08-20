ab8a41d1ad2a30b40c229be2052bf10885a97e9d
Daniel Beff <daniel.beff@gmail.com>
2016-08-20 00:23:02 -0300

    create tabDelete action reducer

--- app/assets/scripts/reducers/tabs.js
+++ app/assets/scripts/reducers/tabs.js
@@ -1,9 +1,13 @@
-import {TAB_ADD} from '../actions/index';
+import {TAB_ADD,TAB_DELETE} from '../actions/index';
 
 export default function(state = [],action) {
     switch (action.type){
         case TAB_ADD:
             return [...state,action.payload];
+        case TAB_DELETE:
+            const tabs = state.slice();
+            tabs.splice(action.payload.tab,1);
+            return tabs;
     }
     return state;
 }
