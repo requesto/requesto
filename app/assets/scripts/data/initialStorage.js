90d15f2c01902766eac060493c4c4b8c620dc4f9
Daniel Beff <daniel.beff@gmail.com>
2016-09-21 13:30:25 -0300

    improve initial storage

--- app/assets/scripts/data/initialStorage.js
+++ app/assets/scripts/data/initialStorage.js
@@ -3,12 +3,44 @@ export const initialStorage= {
     folders: [{
         name:"Blog example",
         id: 1,
-        items:[
-        {
+        items:[{
             name:"/posts",
             description:"Get posts",
             type:"GET",
             url:"http://jsonplaceholder.typicode.com/posts"
+        },{
+            name:"/posts?userId=1",
+            description:"Get posts filter by userId",
+            type:"GET",
+            url:"http://jsonplaceholder.typicode.com/posts",
+            params:{
+                "userId":"1"
+            }
+        },{
+            name:"/posts",
+            description:"Get posts filter by userId",
+            type:"POST",
+            url:"http://jsonplaceholder.typicode.com/posts",
+            body:{
+                title: "foo",
+                body: "bar",
+                userId: "1"
+            }
+        },{
+            name:"/posts/1",
+            description:"Patch post",
+            type:"PUT",
+            url:"http://jsonplaceholder.typicode.com/posts/1"
+        },{
+            name:"/posts/1",
+            description:"Delete post",
+            type:"DELETE",
+            url:"http://jsonplaceholder.typicode.com/posts/1"
+        },{
+            name:"/posts/1",
+            description:"Patch post",
+            type:"PATCH",
+            url:"http://jsonplaceholder.typicode.com/posts/1"
         }]
     }]
 }
