10582ebae5eed444f441c7dcee562da2f256f52e
Daniel Beff <daniel.beff@gmail.com>
2016-11-12 15:33:53 -0200

    resolve code identation

--- app/assets/scripts/reducers/folders.js
+++ app/assets/scripts/reducers/folders.js
@@ -1,21 +1,23 @@
 import File from "../libs/file";
-import {FOLDER_ADD,
-        FOLDER_DELETE,
-        FOLDER_ITEM_ADD,
-        FOLDER_ITEM_DELETE,
-        FOLDER_ITEM_EDIT,
-        FOLDER_FETCH} from '../actions/index';
+import {
+    FOLDER_ADD,
+    FOLDER_DELETE,
+    FOLDER_ITEM_ADD,
+    FOLDER_ITEM_DELETE,
+    FOLDER_ITEM_EDIT,
+    FOLDER_FETCH
+} from '../actions/index';
 
-export default function(state = [],action) {
+export default function(state = [], action) {
     const file = new File();
     var folders = [];
-    switch (action.type){
+    switch (action.type) {
         case FOLDER_FETCH:
             return action.payload;
 
         case FOLDER_ADD:
-            folders = [...state,action.payload]
-            file.loadStorageFile(function (data){
+            folders = [...state, action.payload]
+            file.loadStorageFile(function(data) {
                 data.folders = folders
                 file.updateStorageFile(data);
             });
@@ -23,36 +25,36 @@ export default function(state = [],action) {
 
         case FOLDER_DELETE:
             folders = state.slice();
-            folders.splice(action.payload.folder,1);
-            file.loadStorageFile(function (data){
+            folders.splice(action.payload.folder, 1);
+            file.loadStorageFile(function(data) {
                 data.folders = folders
                 file.updateStorageFile(data);
             });
             return folders;
 
         case FOLDER_ITEM_ADD:
-            folders = state.map(function(folder){
-                if (folder.id == action.payload.folder){
+            folders = state.map(function(folder) {
+                if (folder.id == action.payload.folder) {
                     folder.items.push(action.payload.item)
                 }
                 return folder;
             });
 
-            file.loadStorageFile(function (data){
+            file.loadStorageFile(function(data) {
                 data.folders = folders
                 file.updateStorageFile(data);
             });
             return folders;
 
         case FOLDER_ITEM_DELETE:
-            folders = state.map(function(folder,index){
-                if (index == action.payload.folder){
-                    folder.items.splice(action.payload.item,1);
+            folders = state.map(function(folder, index) {
+                if (index == action.payload.folder) {
+                    folder.items.splice(action.payload.item, 1);
                 }
                 return folder;
             });
 
-            file.loadStorageFile(function (data){
+            file.loadStorageFile(function(data) {
                 data.folders = state
                 file.updateStorageFile(data);
             });
@@ -61,10 +63,10 @@ export default function(state = [],action) {
 
             return folders;
         case FOLDER_ITEM_EDIT:
-            folders = state.map(function(folder){
-                folder.items.forEach(function(item,index){
-                    if (item.id === action.payload.item.id){
-                        console.log("find id:",item.id," with url: ",action.payload.item.url);
+            folders = state.map(function(folder) {
+                folder.items.forEach(function(item, index) {
+                    if (item.id === action.payload.item.id) {
+                        console.log("find id:", item.id, " with url: ", action.payload.item.url);
                         folder.items[index] = action.payload.item;
                     }
                 })
@@ -73,7 +75,7 @@ export default function(state = [],action) {
 
             console.log(folders);
 
-            file.loadStorageFile(function (data){
+            file.loadStorageFile(function(data) {
                 data.folders = state
                 file.updateStorageFile(data);
             });
